a5216bcde2568516a68927c5606c433c071913d1
Eli Holzman <eli770440@gmail.com>
2024-06-22 20:56:19 +0300

    add basename to HashRouter

--- src/components/cards/NavbarComp.jsx
+++ src/components/cards/NavbarComp.jsx
@@ -181,7 +181,7 @@ const NavbarComp = ({ toggleDarkMode }) => {
 
     return (
         <div>
-           <HashRouter>
+            <HashRouter basename={process.env.PUBLIC_URL}>
                 {isVisibleNav &&
                     ['md'].map((expand) => (
                         <Navbar key={expand} expand={expand} sticky="top" className="bg-body-tertiary mb-3 header"
@@ -364,7 +364,7 @@ const NavbarComp = ({ toggleDarkMode }) => {
                         </Nav.Link>
                     </Nav.Item>
                 </Nav>
-</HashRouter>
+            </HashRouter>
         </div >
     )
 }
